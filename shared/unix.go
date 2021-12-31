46d237320f6fbd87a904b74901be094cddf3aa25
abdfnx <abdfnabd@gmail.com>
2021-12-31 16:48:34 +0300

    use `doko/log` package in unix shared with new function

--- shared/unix.go
+++ shared/unix.go
@@ -4,8 +4,12 @@
 package shared
 
 import (
+	"os"
+	"os/signal"
 	"syscall"
 	"unsafe"
+
+	logger "github.com/abdfnx/doko/log"
 )
 
 func getTermSize(fd uintptr) (int, int) {
@@ -21,4 +25,37 @@ func getTermSize(fd uintptr) (int, int) {
 	)
 
 	return int(sz.cols), int(sz.rows)
+}
+
+func IsTermWindowSizeBiggerThanZero() bool {
+	out, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
+	if err != nil {
+		logger.Logger.Error(err)
+		return false
+	}
+
+	defer out.Close()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGWINCH, syscall.SIGINT)
+
+	for {
+		// check terminal window size
+		termw, termh := getTermSize(out.Fd())
+		if termw > 0 && termh > 0 {
+			return true
+		}
+
+		select {
+			case signal := <-signalCh:
+				switch signal {
+					// when the terminal window size is changed
+					case syscall.SIGWINCH:
+						continue
+					// use `ctrl + c`` to cancel
+					case syscall.SIGINT:
+						return false
+				}
+		}
+	}
 }
