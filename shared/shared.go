99188e52e3f805951acfbf89bde1c1d69900b5f0
abdfnx <abdfnabd@gmail.com>
2021-12-31 17:01:45 +0300

    add `GetEnv`, `ParseDateToString`, `ParseSizeToString` funcs

--- shared/shared.go
+++ shared/shared.go
@@ -1,9 +1,13 @@
 package shared
 
 import (
-	"bytes"
-	"encoding/json"
+	"os"
+	"fmt"
 	"sort"
+	"time"
+	"bytes"
+	"strings"
+	"encoding/json"
 )
 
 // StructToJSON -> convert any struct to json.
@@ -23,4 +27,28 @@ func StructToJSON(i interface{}) string {
 func SortKeys(keys []string) []string {
 	sort.Strings(keys)
 	return keys
+}
+
+// GetEnv -> get os environment.
+func GetEnv(env string) string {
+	keyval := strings.SplitN(env, "=", 2)
+
+	if keyval[1][:1] == "$" {
+		keyval[1] = os.Getenv(keyval[1][1:])
+		return strings.Join(keyval, "=")
+	}
+
+	return env
+}
+
+// ParseDateToString -> parse date to string.
+func ParseDateToString(unixtime int64) string {
+	t := time.Unix(unixtime, 0)
+	return t.Format("2006/01/02 15:04:05")
+}
+
+// ParseSizeToString -> parse size to string.
+func ParseSizeToString(size int64) string {
+	mb := float64(size) / 1024 / 1024
+	return fmt.Sprintf("%.1fMB", mb)
 }
