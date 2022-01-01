3108a6c58ceba72b1ade7ee7201525529eec162b
abdfnx <abdfnabd@gmail.com>
2022-01-01 12:54:31 +0300

    new go modules, start build docker client in `docker` package

---
+++ docker/client.go
@@ -0,0 +1,71 @@
+package docker
+
+import (
+	"os"
+
+	"github.com/docker/docker/client"
+)
+
+// Client docker client
+var Client *Docker
+
+// Docker docker client
+type Docker struct {
+	*client.Client
+}
+
+// ClientConfig docker client config
+type ClientConfig struct {
+	endpoint   string
+	certPath   string
+	keyPath    string
+	caPath     string
+	apiVersion string
+}
+
+// NewClientConfig create docker client config
+func NewClientConfig(endpoint, cert, key, ca, apiVersion string) *ClientConfig {
+	return &ClientConfig{
+		endpoint:   endpoint,
+		certPath:   cert,
+		keyPath:    key,
+		caPath:     ca,
+		apiVersion: apiVersion,
+	}
+}
+
+// NewDocker create new docker client
+func NewDocker(config *ClientConfig) *Docker {
+	if os.Getenv("DOCKER_HOST") != "" {
+		client, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion(config.apiVersion))
+		if err != nil {
+			panic(err)
+		}
+
+		return &Docker{client}
+	}
+
+	if config.caPath != "" &&
+		config.certPath != "" &&
+		config.keyPath != "" {
+		client, err := client.NewClientWithOpts(client.WithTLSClientConfig(config.caPath, config.certPath, config.keyPath),
+			client.WithHost(config.endpoint),
+			client.WithVersion(config.apiVersion))
+
+		if err != nil {
+			panic(err)
+		}
+
+		return &Docker{client}
+	}
+
+	// default api version 1.39
+	client, err := client.NewClientWithOpts(client.WithHost(config.endpoint), client.WithVersion(config.apiVersion))
+	if err != nil {
+		panic(err)
+	}
+
+	Client = &Docker{client}
+
+	return Client
+}
--- go.mod
+++ go.mod
@@ -9,12 +9,23 @@ require (
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/containerd/containerd v1.5.8 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
-	gotest.tools/v3 v3.0.3 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
