f343f06f07e191f5272f18d7670ad10100041eac
David Tomson <90329278+david-tomson@users.noreply.github.com>
2022-01-07 13:55:20 +0300

    if `buildVersion` is empty then don't check new versions

--- core/checker/checker.go
+++ core/checker/checker.go
@@ -17,10 +17,14 @@ func Check(buildVersion string) {
 	cmdFactory := factory.New()
 	stderr := cmdFactory.IOStreams.ErrOut
 
+	if buildVersion == "" {
+		buildVersion = "unknown"
+	}
+
 	latestVersion := api.GetLatest()
 	isFromHomebrewTap := isUnderHomebrew()
 	isFromGo := isUnderGo()
-	isFromUsrBinDir := isUnderUsr()
+	isFromUsr := isUnderUsr()
 	isFromAppData := isUnderAppData()
 
 	var command = func() string {
@@ -28,7 +32,7 @@ func Check(buildVersion string) {
 			return "brew upgrade doko"
 		} else if isFromGo {
 			return "go get -u github.com/abdfnx/doko"
-		} else if isFromUsrBinDir {
+		} else if isFromUsr {
 			return "curl -fsSL https://git.io/doko | bash"
 		} else if isFromAppData {
 			return "iwr -useb https://git.io/doko-win | iex"
@@ -37,14 +41,16 @@ func Check(buildVersion string) {
 		return ""
 	}
 
-	if buildVersion != latestVersion && gjson.Get(tools.SettingsContent(), "dk_settings.show_update").String() != "false" {
-		fmt.Fprintf(stderr, "%s %s → %s\n",
-		ansi.Color("There's a new version of ", "yellow") + ansi.Color("doko", "cyan") + ansi.Color(" is avalaible:", "yellow"),
-		ansi.Color(buildVersion, "cyan"),
-		ansi.Color(latestVersion, "cyan"))
-
-		if command() != "" {
-			fmt.Fprintf(stderr, ansi.Color("To upgrade, run: %s\n", "yellow"), ansi.Color(command(), "black:white"))
+	if buildVersion != "unknown" {
+		if buildVersion != latestVersion && gjson.Get(tools.SettingsContent(), "dk_settings.show_update").String() != "false" {
+			fmt.Fprintf(stderr, "%s %s → %s\n",
+			ansi.Color("There's a new version of ", "yellow") + ansi.Color("doko", "cyan") + ansi.Color(" is avalaible:", "yellow"),
+			ansi.Color(buildVersion, "cyan"),
+			ansi.Color(latestVersion, "cyan"))
+	
+			if command() != "" {
+				fmt.Fprintf(stderr, ansi.Color("To upgrade, run: %s\n", "yellow"), ansi.Color(command(), "black:white"))
+			}
 		}
 	}
 }
